26ace349174ee626d878532d45a8aad49bb5b164
agftech <agiraof@gmail.com>
2020-11-22 13:58:21 -0800

    :package: feat: replace ID with UUID and finish the category resource

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, Traits\Uuid;
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = ['id' => 'string'];
 }
